e47632c2216745fc7ee4559a967d62fb4f74e63a
Cirrus CI <support@cirruslabs.org>
2023-04-15 08:00:20 +0000

    added todos model

--- lib/repository/todo_repository.dart
+++ lib/repository/todo_repository.dart
@@ -1,18 +1,21 @@
 import 'dart:convert';
 
-import 'package:flutter_vorlesung/repository/todo_model.dart';
+import 'package:flutter_vorlesung/repository/todos_model.dart';
 import 'package:http/http.dart' as http;
 
 class TodoRepository {
-  Future<List<TodoModel>> getTodos() async {
+  Future<Todos> getTodos() async {
     final response = await http.get(Uri.https('dummyjson.com', 'todos'));
     final body = jsonDecode(response.body) as Map<String, dynamic>;
 
-    final result = <TodoModel>[];
+    final result = Todos.fromJson(body);
+    //final result = <TodoModel>[];
 
+    /*
     for (final Map<String, dynamic> todo in (body['todos']) as List<dynamic>){
       result.add(TodoModel(todo['id'], todo['todo'], todo['completed']));
     }
+    */
     return result;
   }
 }
